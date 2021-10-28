8796759828b8a82ded2b85dbd35a7ce5ce89f1e3
Federico Cecchinato <federicocech@gmail.com>
2021-10-28 03:20:55 +0100

    moved animate func in Algorithm class

--- src/lib/algorithms/Algorithm.ts
+++ src/lib/algorithms/Algorithm.ts
@@ -4,5 +4,28 @@ import { Pair } from "./BFS";
 export abstract class Algorirthm {
   abstract solve(...params: any): Pair<Node[]>;
 
-  abstract animate(...params: any): void;
+  animate(visistedNodes: Node[], shortestPath: Node[]): void {
+    for (let i = 0; i <= visistedNodes.length; i++) {
+      if (i === visistedNodes.length) {
+        setTimeout(() => this.animateShortestPath(shortestPath), 10 * i);
+        return;
+      }
+      const v = visistedNodes[i];
+      setTimeout(() => {
+        const node_el = document.getElementById(v.toKey());
+        if (node_el) node_el.className = "transform scale w-7 h-7 bg-blue-500";
+      }, i * 10);
+    }
+  }
+
+  private animateShortestPath(shortestPath: Node[]) {
+    for (let i = 0; i < shortestPath.length; i++) {
+      const v = shortestPath[i];
+      setTimeout(() => {
+        const node_el = document.getElementById(v.toKey());
+        if (node_el)
+          node_el.className = "transform scale w-7 h-7 bg-yellow-500";
+      }, i * 50);
+    }
+  }
 }
--- src/lib/algorithms/BFS.ts
+++ src/lib/algorithms/BFS.ts
@@ -16,31 +16,6 @@ export class Pair<T> {
 }
 
 export class BFS extends Algorirthm {
-  animate(visistedNodes: Node[], shortestPath: Node[]): void {
-    for (let i = 0; i <= visistedNodes.length; i++) {
-      if (i === visistedNodes.length) {
-        setTimeout(() => this.animateShortestPath(shortestPath), 10 * i);
-        return;
-      }
-      const v = visistedNodes[i];
-      setTimeout(() => {
-        const node_el = document.getElementById(v.toKey());
-        if (node_el) node_el.className = "transform scale w-7 h-7 bg-blue-500";
-      }, i * 10);
-    }
-  }
-
-  private animateShortestPath(shortestPath: Node[]) {
-    for (let i = 0; i < shortestPath.length; i++) {
-      const v = shortestPath[i];
-      setTimeout(() => {
-        const node_el = document.getElementById(v.toKey());
-        if (node_el)
-          node_el.className = "transform scale w-7 h-7 bg-yellow-500";
-      }, i * 50);
-    }
-  }
-
   solve(grid: Grid, startNode: Node, endNode: Node): Pair<Node[]> {
     const path = {};
     const queue: Node[] = [startNode];
